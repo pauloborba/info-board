06ceb61810bf5ccfe3151c0396410b5966a2d506
GabrielBarbosaGV <gbgv@cin.ufpe.br>
2018-06-12 21:28:02 -0300

    Created vote-list component base.

--- gui/info-board/src/app/app.module.ts
+++ gui/info-board/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { UserListComponent } from './user-list/user-list.component';
+import { VoteListComponent } from './vote-list/vote-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserListComponent
+    UserListComponent,
+    VoteListComponent
   ],
   imports: [
     BrowserModule
