2e3fe9f746ca456dcd260cca9be09be0ae0d656e
Franclin Cabral <franclincabral@gmail.com>
2018-06-16 18:28:05 -0300

    Modyfing structure and adding structure to database and routing

--- server/index.ts
+++
@@ -1,9 +0,0 @@
-import bodyParser = require("body-parser");
-import * as http from "http";
-import app from './src/app';
-
-const server: http.Server = http.createServer(app);
-
-server.listen(5000, () => {
-  console.log("Server listening to the port 5000");
-});
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,24 +1,25 @@
 import * as express from 'express';
 import { Express } from 'express';
 import * as bodyParser from "body-parser";
-import * as path from "path";
+import Routes from "./api/route/route";
 
 const app = (): Express => {
-  console.log("Should implement express");
-  const app = express();
+    console.log("Should implement express");
+    const app = express();
 
-  var allowCrossDomain = function(req: any, res: any, next: any) {
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  }
+    const allowCrossDomain = function(req: any, res: any, next: any) {
+        res.header('Access-Control-Allow-Origin', "*");
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.header('Access-Control-Allow-Headers', 'Content-Type');
+        next();
+    };
 
-  app.use(allowCrossDomain);
-  app.use(bodyParser.json());
-  
+    app.use(allowCrossDomain);
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+    Routes(app);
 
-  return app;
-}
+    return app;
+};
 
 export default app();
