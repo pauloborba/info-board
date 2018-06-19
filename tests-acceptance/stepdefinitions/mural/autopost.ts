c9f47556e562af612bee7b1cff748beaddb4c4d7
Nara <nsa2@cin.ufpe.br>
2018-06-18 23:00:26 -0300

    continuando implementação de testes (pequenas alterações em agenda, autopost, favoritos e postfixo)

--- tests-acceptance/stepdefinitions/mural/autopost.ts
+++ tests-acceptance/stepdefinitions/mural/autopost.ts
@@ -3,8 +3,6 @@ import { browser, $, element, ElementArrayFinder, by } from 'protractor';
 let chai = require('chai').use(require('chai-as-promised'));
 let expect = chai.expect;
 
-let sleep = (ms => new Promise(resolve => setTimeout(resolve, ms)));
-
 defineSupportCode(function ({ Given, When, Then }) {
     Given(/^Eu sou um usuário "João" logado no sistema$/, async () => {
         //TODO await verificar que há um usuário logado no sistema
@@ -12,11 +10,13 @@ defineSupportCode(function ({ Given, When, Then }) {
     
     Given(/^eu estou na página "Votações"$/, async () => {
         await browser.get("http://localhost:4200/");
-        await expect(browser.getTitle()).to.eventually.equal('Mural de Informações: Votações');
+        await expect(browser.getTitle()).to.eventually.equal('Mural de Informações');
+        await $("a[name='votacoes']").click();
     })
 
     Given(/^eu vejo uma votação "Votação para pauta da Reunião X"$/, async () => {
-        //TODO await verificar votação do tipo "pauta" na lista de votações que seja sobre a "Reunião X"
+        //TODO await verificar votação do tipo "pauta" na lista de votações que tenha evento "Reunião X"
+        var listaVotacoes : ElementArrayFinder = element.all(by.name('votacaolist'));
     })
 
     Given(/^eu vejo que o tempo de votação para a "Votação para pauta da Reunião X" foi encerrado$/, async () => {
@@ -24,10 +24,12 @@ defineSupportCode(function ({ Given, When, Then }) {
     })
 
     When(/^eu vou para a página "Mural de Informações"$/, async () => {
-        await $("a[name='Mural de Informações']").click();
+        await $("a[name='publicacoes']").click();
     });
 
     Then(/^eu vejo uma publicação "Post Pauta Reunião X" informando o tema escolhido para a pauta da "Reunião X"$/, async () => {
-        //TODO await verificar publicação na lista de publicações do mural que seja do tipo "pauta de reunião" e que fale sobre a "reunião x"
+        //TODO await verificar publicação na lista de publicações do mural que seja do tipo pauta_reuniao
+        // e que o evento seja a "reunião x" (o id)
+        var listaPublicacoes : ElementArrayFinder = element.all(by.name('publicacaolist'));
     });
 })
--- tests-acceptance/stepdefinitions/mural/favoritos.ts
+++ tests-acceptance/stepdefinitions/mural/favoritos.ts
@@ -3,8 +3,6 @@ import { browser, $, element, ElementArrayFinder, by } from 'protractor';
 let chai = require('chai').use(require('chai-as-promised'));
 let expect = chai.expect;
 
-let sleep = (ms => new Promise(resolve => setTimeout(resolve, ms)));
-
 defineSupportCode(function ({ Given, When, Then }) {
     Given(/^Eu sou um usuário "João" logado no sistema$/, async () => {
         //TODO await verificar que há um usuário logado no sistema
@@ -13,21 +11,25 @@ defineSupportCode(function ({ Given, When, Then }) {
     Given(/^eu estou na página inicial "Mural de Informações"$/, async () => {
         await browser.get("http://localhost:4200/");
         await expect(browser.getTitle()).to.eventually.equal('Mural de Informações');
+        await $("a[name='publicacoes']").click();
     })
 
     Given(/^eu vejo uma publicação "([^\"]*)" exibida no mural$/, async () => {
         //TODO await verificar post na lista de publicações do mural
+        var listaPublicacoes : ElementArrayFinder = element.all(by.name('publicacaolist'));
     })
 
     Given(/eu vejo que a publicação "([^\"]*)" não está sinalizada como favorita$/, async() => {
         //TODO await verificar que o post selecionado acima não está na lista de favoritos
+        var listaFavoritos : ElementArrayFinder = element.all(by.name('favoritoslist'));
     })
 
     When(/^eu seleciono a opção de favoritar a publicação "([^\"]*)"$/, async () => {
-        await $("a[name='Favoritar']").click();
+        await $("a[name='favoritar']").click();
     });
 
     Then(/^eu vejo uma mensagem informando que a publicação "([^\"]*)" foi adicionada à minha lista de favoritos$/, async () => {
         //TODO await verificar que o post selecionado acima está na lista de favoritos
+        var listaFavoritos : ElementArrayFinder = element.all(by.name('favoritoslist'));
     });
 })
